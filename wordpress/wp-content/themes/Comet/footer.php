944dad0d0b2a338dd80325e5c4760e3812ff8545
Sheng Xiao <sheng26v@gmail.com>
2020-07-19 20:07:59 -0700

    feat: footer area with divider line

--- wordpress/wp-content/themes/Comet/footer.php
+++ wordpress/wp-content/themes/Comet/footer.php
@@ -1,3 +1,11 @@
-<?php wp_footer(); ?>
-</body>
+          </div>
+        </div>
+      <div class="site-footer">
+        <div class="wrapper wrapper-footer">
+          <div div class="footer-divider"></div>
+        </div>
+      </div>
+    </div>
+    <?php wp_footer(); ?>
+  </body>
 </html>
