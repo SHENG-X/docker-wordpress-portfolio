e498bc4bff10fc2df7fa04c10a3a0b2f653b679b
Sheng Xiao <sheng26v@gmail.com>
2020-07-19 20:08:38 -0700

    feat: front page with intro, pic, skills, projects content area

--- wordpress/wp-content/themes/Comet/front-page.php
+++ wordpress/wp-content/themes/Comet/front-page.php
@@ -1,11 +1,58 @@
 <?php get_header() ?>
 
-<div class="container">
-  <h1>
-    <?php the_title(); ?>
-  </h1>
-
-  <?php get_template_part( 'inc/section', 'content'); ?>
+<div class="font-page-content">
+  <div class="row">
+    <div class="intro col-md-8">
+      <div>
+        <?php if(is_active_sidebar('front-page-intro')) :?>
+          <?php dynamic_sidebar('front-page-intro') ?>
+        <?php endif; ?>
+      </div>
+      <div class="social-media">
+        <?php if(is_active_sidebar('front-page-social')) :?>
+          <?php dynamic_sidebar('front-page-social') ?>
+        <?php endif; ?>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <?php if(is_active_sidebar('front-page-pic')) :?>
+        <?php dynamic_sidebar('front-page-pic') ?>
+      <?php endif; ?>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <h4>
+        Skills
+      </h4>
+    </div>
+    <div class="col-md-12">
+      <div class="skills-container">
+        <div class="skill-item">
+          Frontend
+        </div>
+        <div class="skill-item">
+          Backend
+        </div>
+        <div class="skill-item">
+          Database
+        </div>
+        <div class="skill-item">
+          Version Control
+        </div>
+        <div class="skill-item">
+          Project Management Tools
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <?php if(is_active_sidebar('front-page-projects')) :?>
+        <?php dynamic_sidebar('front-page-projects') ?>
+      <?php endif; ?>
+    </div>
+  </div>
 </div>
 
 <?php get_footer() ?>
