d613e05f16c4cf6e5213cd576c9681914754fa51
Sheng Xiao <sheng26v@gmail.com>
2020-07-21 14:39:32 -0700

    chore: moved skills section to its own template

---
+++ wordpress/wp-content/themes/Comet/inc/section-skills.php
@@ -0,0 +1,118 @@
+<div class="row">
+  <div class="col-md-12">
+    <h4>
+      Skills
+    </h4>
+  </div>
+  <div class="col-md-12">
+    <div class="skills-container">
+
+    <div class="skill-item front-end">
+      <div class="title">
+        Frontend 
+      </div>
+      <div class="content">
+        <div>
+          JavaScript
+        </div>
+        <div>
+          TypeScript
+        </div>
+        <div>
+          HTML
+        </div>
+        <div>
+          CSS
+        </div>
+        <div>
+          SCSS
+        </div>
+        <div>
+          React.js
+        </div>
+        <div>
+          React Native
+        </div>
+        <div>
+          Vue.js
+        </div>
+        <div>
+          jQuery
+        </div>
+      </div>
+    </div>
+
+    <div class="skill-item back-end">
+      <div class="title">
+        Backend
+      </div>
+      <div class="content">
+        <div>
+          Node.js
+        </div>
+        <div>
+          Django
+        </div>
+        <div>
+          .Net
+        </div>
+        <div>
+          PHP
+        </div>
+      </div>
+    </div>
+
+    <div class="skill-item database">
+      <div class="title">
+        Database
+      </div>
+      <div class="content">
+        <div>
+          MySQL
+        </div>
+        <div>
+          MongoDB
+        </div>
+        <div>
+          MS SQL
+        </div>
+        <div>
+          SQLite
+        </div>
+      </div>
+    </div>
+
+    <div class="skill-item version-control">
+      <div class="title">
+        Version Control
+      </div>
+      <div class="content">
+        <div>
+          GIT
+        </div>
+        <div>
+          TFS
+        </div>
+      </div>
+    </div>
+
+    <div class="skill-item project-management">
+      <div class="title">
+        Project MGMT Tools
+      </div>
+      <div class="content">
+        <div>
+          Azure DevOps
+        </div>
+        <div>
+          GitHub
+        </div>
+        <div>
+          MS Teams
+        </div>
+      </div>
+    </div>
+
+    </div>
+  </div>
+</div>
