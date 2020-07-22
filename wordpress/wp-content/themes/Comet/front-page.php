72f98e03ddc3f3437d952a30ba639ec7928d61f4
Sheng Xiao <sheng26v@gmail.com>
2020-07-21 17:48:07 -0700

    chore: new design of the page

--- wordpress/wp-content/themes/Comet/front-page.php
+++ wordpress/wp-content/themes/Comet/front-page.php
@@ -1,25 +1,6 @@
 <?php get_header() ?>
 
 <div class="font-page-content">
-  <div class="row">
-    <div class="intro col-md-8">
-      <div>
-        <?php if(is_active_sidebar('front-page-intro')) :?>
-          <?php dynamic_sidebar('front-page-intro') ?>
-        <?php endif; ?>
-      </div>
-      <div class="social-media">
-        <?php if(is_active_sidebar('front-page-social')) :?>
-          <?php dynamic_sidebar('front-page-social') ?>
-        <?php endif; ?>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <?php if(is_active_sidebar('front-page-pic')) :?>
-        <?php dynamic_sidebar('front-page-pic') ?>
-      <?php endif; ?>
-    </div>
-  </div>
   <?php get_template_part('inc/section', 'skills')?>
   <?php get_template_part('inc/section', 'portfolio')?>
 </div>
--- wordpress/wp-content/themes/Comet/header.php
+++ wordpress/wp-content/themes/Comet/header.php
@@ -8,11 +8,30 @@
   <body>
     <div id="container">
       <header class="site-header clearfix" role="banner">
-        <div class="wrapper wrapper-header clearfix">
-          <?php 
-            get_template_part('inc/section', 'main-nav');
-          ?>
-          <div class="site-header-divider"></div>
+        <?php 
+          get_template_part('inc/section', 'main-nav');
+        ?>
+        <div class="intro-container">
+          <div class="wrapper wrapper-main clearfix">
+            <div class="row">
+              <div class="intro col-md-8">
+                <div class="leading-label">
+                  Hello, my name is
+                </div>
+                <h2 class="mt-0 display-4 font-weight-bold">
+                  Sheng Xiao
+                </h2>
+                <div class="bio mb-3">
+                  I am a full stack web application developer.  I have over two years enterprise web application development experiences. I am enthusiasm for coding and programming. Don’t hesitate to contact me if you need some help.
+                </div>
+              </div>
+              <div class="col-md-4">
+                <?php if(is_active_sidebar('front-page-pic')) :?>
+                  <?php dynamic_sidebar('front-page-pic') ?>
+                <?php endif; ?>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
       <div class="site-main">
