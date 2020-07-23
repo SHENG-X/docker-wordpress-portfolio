26223140d943384879ffa334cc95d4f4df5f1122
Sheng Xiao <sheng26v@gmail.com>
2020-07-23 15:13:32 -0700

    chore: project short use shared css and add small padding on x

--- wordpress/wp-content/themes/Comet/inc/section-project-short.php
+++ wordpress/wp-content/themes/Comet/inc/section-project-short.php
@@ -1,16 +1,15 @@
-<div class="project-short">
+<div class="project-short comet-card">
   <?php if (has_post_thumbnail()): ?>
     <img
       src="<?php the_post_thumbnail_url('blog-small'); ?>"
       alt="<?php the_title(); ?>"
     />
   <?php endif;?>
-  <div class="details">
+  <div class="details px-3">
     <h5>
       <?php the_title(); ?>
     </h5>
     <?php the_field('bref'); ?>
-    
   </div>
   <div class="view-project">
     <a
