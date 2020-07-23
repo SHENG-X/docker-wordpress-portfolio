2a17458cba49071282a8bfd47adefc02466c8afd
Sheng Xiao <sheng26v@gmail.com>
2020-07-23 15:07:03 -0700

    feat: footer section for contact

--- wordpress/wp-content/themes/Comet/footer.php
+++ wordpress/wp-content/themes/Comet/footer.php
@@ -2,6 +2,23 @@
           </div>
         </div>
       <footer class="site-footer">
+        <div class="hire-me py-5">
+          <h4 class="section-title font-weight-bold text-center mb-3">
+            Interested in hiring me for your project?
+          </h4>
+          <div class="container text-center">
+            Looking for an experienced full-stack developer to 
+            build your web app or ship your software product? To 
+            start an initial chat, just drop me an email at 
+            <a href="mailto:sheng.x.work@gmail.com">sheng.x.work@gmail.com</a> or use the form on the contact 
+            page.
+          </div>
+          <div class="container flex justify-center items-center mt-3">
+            <button class="btn btn-primary">
+              Talk to me
+            </button>
+          </div>
+        </div>
         <div class="copy-right">
           Copyright © <?php echo date("Y"); ?> SHENG XIAO. All Rights Reserved.
         </div>
