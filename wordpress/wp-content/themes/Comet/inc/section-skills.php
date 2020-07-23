010d745f2d9117ee9dc03bfce173fddf91023e3e
Sheng Xiao <sheng26v@gmail.com>
2020-07-22 17:16:50 -0700

    chore: implemented a new skills design

--- wordpress/wp-content/themes/Comet/inc/section-skills.php
+++ wordpress/wp-content/themes/Comet/inc/section-skills.php
@@ -3,116 +3,199 @@
     <h4 class="section-title font-weight-bold text-center mb-3">
       Skills Overview
     </h4>
-  </div>
-  <div class="col-md-12">
-    <div class="skills-container">
-
-    <div class="skill-item front-end">
-      <div class="title">
-        Frontend 
-      </div>
-      <div class="content">
-        <div>
-          JavaScript
-        </div>
-        <div>
-          TypeScript
-        </div>
-        <div>
-          HTML
-        </div>
-        <div>
-          CSS
-        </div>
-        <div>
-          SCSS
-        </div>
-        <div>
-          React.js
-        </div>
-        <div>
-          React Native
-        </div>
-        <div>
-          Vue.js
-        </div>
-        <div>
-          jQuery
-        </div>
+    <div class="description-container">
+      <div class="description">
+        I have more than 8 years' experience building rich web applications for clients all over the world. Below is a quick overview of my main technical skill sets and tools I use. Want to find out more about my experience?
       </div>
     </div>
-
-    <div class="skill-item back-end">
+  </div>
+  <div class="col-md-12">
+  <div class="skills-container">
+    <div class="skills-item">
+      <div class="icon">
+        <img src="https://img.icons8.com/cotton/64/000000/web-design.png"/>
+      </div>
+      <div class="title">
+        Frontend
+      </div>
+      <div class="content">
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            React / Redux
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            React Native
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Flutter
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Vue / Vuex
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            JavaScript / TypeScript
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            HTML / CSS / SCSS
+          </div>
+        </div>
+        
+      </div>
+    </div>
+    
+    <div class="skills-item">
+      <div class="icon">
+        <img src="https://img.icons8.com/cute-clipart/64/000000/server.png"/>
+      </div>
       <div class="title">
         Backend
       </div>
       <div class="content">
-        <div>
-          Node.js
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Python / Django
+          </div>
         </div>
-        <div>
-          Django
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            JavaScript / Node.js
+          </div>
         </div>
-        <div>
-          .Net
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            C# / .Net
+          </div>
         </div>
-        <div>
-          PHP
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            PHP
+          </div>
         </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            My SQL / MS SQL / SQLite
+          </div>
+        </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            MongoDB
+          </div>
+        </div>
+        
       </div>
     </div>
-
-    <div class="skill-item database">
+    
+    <div class="skills-item">
+      <div class="icon">
+        <img src="https://img.icons8.com/dusk/64/000000/administrative-tools.png"/>
+      </div>
       <div class="title">
-        Database
+        Others
       </div>
       <div class="content">
-        <div>
-          MySQL
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Azure DevOps
+          </div>
         </div>
-        <div>
-          MongoDB
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Git / GitHub
+          </div>
         </div>
-        <div>
-          MS SQL
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            WordPress
+          </div>
         </div>
-        <div>
-          SQLite
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Docker
+          </div>
         </div>
+        
+        <div class="item">
+          <div class="item-icon">
+            <svg class="svg-inline--fa fa-check fa-w-16 mr-2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+          </div>
+          <div class="item-text">
+            Jest
+          </div>
+        </div>
+        
       </div>
     </div>
-
-    <div class="skill-item version-control">
-      <div class="title">
-        Version Control
-      </div>
-      <div class="content">
-        <div>
-          GIT
-        </div>
-        <div>
-          TFS
-        </div>
-      </div>
-    </div>
-
-    <div class="skill-item project-management">
-      <div class="title">
-        Project MGMT Tools
-      </div>
-      <div class="content">
-        <div>
-          Azure DevOps
-        </div>
-        <div>
-          GitHub
-        </div>
-        <div>
-          MS Teams
-        </div>
-      </div>
-    </div>
-
-    </div>
+  </div>
   </div>
 </div>
