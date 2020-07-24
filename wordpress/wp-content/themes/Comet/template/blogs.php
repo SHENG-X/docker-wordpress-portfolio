95ec7855f61c200bd61566ad97a487526fbd4fb2
Sheng Xiao <sheng26v@gmail.com>
2020-07-23 19:37:33 -0700

    chore: wrap blog section in a container

--- wordpress/wp-content/themes/Comet/template/blogs.php
+++ wordpress/wp-content/themes/Comet/template/blogs.php
@@ -4,32 +4,34 @@ Template Name: Blogs
 */
 ?>
 <?php get_header() ?>
-  <div class="row">
-    <div class="col-md-3">
-      <?php if(is_active_sidebar('blog-page-sidebar')) :?>
-        <?php dynamic_sidebar('blog-page-sidebar') ?>
-      <?php endif; ?>
-    </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <?php if(is_active_sidebar('blog-page-sidebar')) :?>
+          <?php dynamic_sidebar('blog-page-sidebar') ?>
+        <?php endif; ?>
+      </div>
 
-    <div class="col-md-9">
-      <?php
-        // show all published blogs
-        $args = array(
-          'post_type'=> 'post',
-          'orderby'    => 'ID',
-          'post_status' => 'publish',
-          'order'    => 'DESC',
-          'posts_per_page' => -1 // this will retrive all the post that is published 
-          );
-          $result = new WP_Query( $args );
-        if ( $result-> have_posts() ) :
-            while ( $result->have_posts() ) : 
-              $result->the_post();
-              get_template_part( 'inc/section', 'blog-short');
-            endwhile;
-        endif; 
-        wp_reset_postdata(); 
-      ?>
+      <div class="col-md-9">
+        <?php
+          // show all published blogs
+          $args = array(
+            'post_type'=> 'post',
+            'orderby'    => 'ID',
+            'post_status' => 'publish',
+            'order'    => 'DESC',
+            'posts_per_page' => -1 // this will retrive all the post that is published 
+            );
+            $result = new WP_Query( $args );
+          if ( $result-> have_posts() ) :
+              while ( $result->have_posts() ) : 
+                $result->the_post();
+                get_template_part( 'inc/section', 'blog-short');
+              endwhile;
+          endif; 
+          wp_reset_postdata(); 
+        ?>
+      </div>
     </div>
   </div>
 <?php get_footer() ?>
