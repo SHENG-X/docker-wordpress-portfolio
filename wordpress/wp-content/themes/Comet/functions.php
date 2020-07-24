465b0910012e43b970a10177e87f6310fdc20a8b
Sheng Xiao <sheng26v@gmail.com>
2020-07-24 15:50:16 -0700

    feat: set excerpt length to be 20 characters

--- wordpress/wp-content/themes/Comet/functions.php
+++ wordpress/wp-content/themes/Comet/functions.php
@@ -164,4 +164,15 @@ function comet_project_post_type() {
 }
 add_action('init', 'comet_project_post_type');;
 
+/**
+ * Filter the except length to 20 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wpdocs_custom_excerpt_length( $length ) {
+  return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
+
 ?>
