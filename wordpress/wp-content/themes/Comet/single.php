2581e13917a6ee9e27ff467151d0aef1d670215a
Sheng Xiao <sheng26v@gmail.com>
2020-07-24 17:24:49 -0700

    feat: use comments template in single blog post template

--- wordpress/wp-content/themes/Comet/single.php
+++ wordpress/wp-content/themes/Comet/single.php
@@ -1,5 +1,11 @@
 <?php get_header() ?>
-  <div class="single-content">
+  <div class="single-content container py-5">
     <?php get_template_part( 'inc/section', 'blog-content'); ?>
+    <?php
+      // If comments are open or we have at least one comment, load up the comment template.
+      if ( comments_open() || get_comments_number() ) :
+        comments_template();
+      endif;
+    ?>
   </div>
 <?php get_footer() ?>
