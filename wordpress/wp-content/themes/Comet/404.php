f89df7e8a422bf074cb49c31315959efeb5a3a30
Sheng Xiao <sheng26v@gmail.com>
2020-07-24 17:23:09 -0700

    chore: show a basic 404 page

--- wordpress/wp-content/themes/Comet/404.php
+++ wordpress/wp-content/themes/Comet/404.php
@@ -1,3 +1,27 @@
 <?php get_header() ?>
- 404 Page not found!
+  <div class="container error404-content py-5">
+    <h1>
+      404
+    </h1>
+    <h4 class="entry-title">
+      <?php _e( 'Page Not Found', 'comet' ); ?>
+    </h4>
+
+    <div class="intro-text">
+      <p>
+        <?php _e( 'The page you were looking for could not be found. 
+          It might have been removed, renamed, or did not exist 
+          in the first place.', 'comet' ); ?>
+      </p>
+    </div>
+    <?php
+      get_search_form(
+        array(
+          'label' => __( '404 not found', 'comet' ),
+        )
+      );
+    ?>
+
+  </div><!-- .section-inner -->
+
 <?php get_footer() ?>
