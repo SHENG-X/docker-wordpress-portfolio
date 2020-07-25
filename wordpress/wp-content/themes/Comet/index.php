27c458ccc2628f9af0bc10126279d2a6abf0391f
Sheng Xiao <sheng26v@gmail.com>
2020-07-24 18:14:35 -0700

    chore: use index for blogs instead of blogs template
    - static posts page use index.php for all archived posts

--- wordpress/wp-content/themes/Comet/index.php
+++ wordpress/wp-content/themes/Comet/index.php
@@ -1,3 +1,39 @@
+<?php
+/*
+Template Name: Blogs
+*/
+?>
 <?php get_header() ?>
-
+  <div class="container">
+    <div class="blog-page-title pt-5">
+      <h4>
+        A Blog About Software Development And Life
+      </h4>
+    </div>
+    <div class="blog-page-description">
+      Thanks for visiting my blog
+    </div>
+  </div>
+  <div class="container py-5">
+      <div class="row">
+        <div class="col-md-12 blogs-container">
+          <?php
+            // if there is archived post, get all
+            // and display them in blog short component
+            if (have_posts()) : 
+              while (have_posts()): 
+                the_post();
+                get_template_part( 'inc/section', 'blog-short');
+              endwhile;
+            endif;
+          ?>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <?php the_posts_pagination(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
 <?php get_footer() ?>
