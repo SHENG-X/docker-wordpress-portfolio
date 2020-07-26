3b76ecf458794f9b1c24fb2ef57057915319784c
Sheng Xiao <sheng26v@gmail.com>
2020-07-25 22:31:41 -0700

    chore: adjust blog post layout

--- wordpress/wp-content/themes/Comet/inc/section-blog-content.php
+++ wordpress/wp-content/themes/Comet/inc/section-blog-content.php
@@ -1,61 +1,57 @@
 <?php if (have_posts()) : while (have_posts()): the_post(); ?>
-  <h4>
-    <?php the_title(); ?>
-  </h4>
-  <div>
-    <?php
+  <div class="blog-post-content pb-5">
+    <h4>
+      <?php the_title(); ?>
+    </h4>
+    <div>
+      <?php
         echo get_the_date('j F, Y');
-    ?>
-  </div>
-  <div>
-    <?php if(has_post_thumbnail()) : ?>
-      <img
-        class="img-thumbnail img-fluid"
-        src="<?php the_post_thumbnail_url('blog-large'); ?>"
-        alt="<?php the_title(); ?>"
-      />
-    <?php endif;?>
-  </div>
-  <div>
-    <?php the_content(); ?>
-  </div>
-  <div class="mb-3">
-    <?php
-      $fname = get_the_author_meta('first_name');
-      $lname = get_the_author_meta('last_name');
-      echo "Posted by: "."$fname $lname"
-    ?>
-
-    <?php
-      $tags = get_the_tags();
-      if (!empty($tags)) :
-        foreach($tags as $tag):
-    ?>
-      <a
-        href="<?php echo get_tag_link($tag->term_id); ?>"
-      >
-      <i class="fa fa-tag"></i>
-       <?php echo $tag->name; ?>
-      </a>
-    <?php 
-        endforeach;
-      endif;
-    ?>
-
-    <?php
-      $categories = get_the_category();
-      if (!empty($categories)) :
-        foreach($categories as $category):
-    ?>
-      <a
-        href="<?php echo get_category_link($category->term_id); ?>"
-      >
+      ?>
+    </div>
+    <div>
+      <?php
+        $tags = get_the_tags();
+        if (!empty($tags)) :
+          foreach($tags as $tag):
+      ?>
+        <a
+          href="<?php echo get_tag_link($tag->term_id); ?>"
+        >
         <i class="fa fa-tag"></i>
-        <?php echo $category->name; ?>
-      </a>
-    <?php 
-        endforeach;
-      endif;
-    ?>
+        <?php echo $tag->name; ?>
+        </a>
+      <?php 
+          endforeach;
+        endif;
+      ?>
+
+      <?php
+        $categories = get_the_category();
+        if (!empty($categories)) :
+          foreach($categories as $category):
+      ?>
+        <a
+          href="<?php echo get_category_link($category->term_id); ?>"
+        >
+          <i class="fa fa-tag"></i>
+          <?php echo $category->name; ?>
+        </a>
+      <?php 
+          endforeach;
+        endif;
+      ?>
+    </div>
+    <div class="py-3">
+      <?php if(has_post_thumbnail()) : ?>
+        <img
+          class="img-fluid"
+          src="<?php the_post_thumbnail_url('blog-large'); ?>"
+          alt="<?php the_title(); ?>"
+        />
+      <?php endif;?>
+    </div>
+    <div>
+      <?php the_content(); ?>
+    </div>
   </div>
 <?php endwhile;endif;?>
