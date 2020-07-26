31ee37e76312fac673f835684ac8489c166d1f9a
Sheng Xiao <sheng26v@gmail.com>
2020-07-25 22:29:52 -0700

    chore: new 404 page design

--- wordpress/wp-content/themes/Comet/404.php
+++ wordpress/wp-content/themes/Comet/404.php
@@ -1,27 +1,21 @@
 <?php get_header() ?>
   <div class="container error404-content py-5">
-    <h1>
+    <div class="label404">
       404
-    </h1>
-    <h4 class="entry-title">
-      <?php _e( 'Page Not Found', 'comet' ); ?>
-    </h4>
-
-    <div class="intro-text">
-      <p>
-        <?php _e( 'The page you were looking for could not be found. 
-          It might have been removed, renamed, or did not exist 
-          in the first place.', 'comet' ); ?>
-      </p>
     </div>
-    <?php
-      get_search_form(
-        array(
-          'label' => __( '404 not found', 'comet' ),
-        )
-      );
-    ?>
-
+    <div class="py-5 px-5">
+      <div class="entry-title">
+        <?php _e( 'Sorry, the page you are looking for doesn\'t exist.', 'comet' ); ?>
+      </div>
+      <div class="buttons mt-3">
+        <a class="btn btn-primary" href="/">
+          Go to Home
+        </a>
+        <a class="btn btn-primary" href="/contact">
+          Contact Me
+        </a>
+      </div>
+    </div>
   </div><!-- .section-inner -->
 
 <?php get_footer() ?>
