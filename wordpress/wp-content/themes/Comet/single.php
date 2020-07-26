5089a65005615df9fc6bb4b24b6075a265cb6bf2
Sheng Xiao <sheng26v@gmail.com>
2020-07-26 13:57:51 -0700

    feat: custom previous and next buttons

--- wordpress/wp-content/themes/Comet/single.php
+++ wordpress/wp-content/themes/Comet/single.php
@@ -1,7 +1,32 @@
 <?php get_header() ?>
   <div class="single-content container py-5">
     <?php get_template_part( 'inc/section', 'blog-content'); ?>
-    <?php
+    
+    <div class="prv-next-buttons mb-5">
+      <?php 
+        previous_post_link('
+          <div class="prev-post">
+            <span class="btn btn-primary">
+              <i class="fa fa-arrow-left" aria-hidden="true"></i>
+              %link
+            </span>
+          </div>', 
+          __('Previous', 'domain')
+        ); 
+      ?>    
+      <?php
+        next_post_link('
+          <div class="next-post">
+            <span class="btn btn-primary">
+            %link
+            <i class="fa fa-arrow-right" aria-hidden="true"></i>
+            </span>
+          </div>', 
+        __('Next', 'domain'));
+      ?>
+    </div>
+
+   <?php
       // If comments are open or we have at least one comment, load up the comment template.
       if ( comments_open() || get_comments_number() ) :
         comments_template();
